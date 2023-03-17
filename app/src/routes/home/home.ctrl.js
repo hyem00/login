926e9b3ab9aa721cfde9a336e7d848f270cb9c0e
hyem00 <hemi1738@naver.com>
2023-03-17 16:32:16 +0900

    로그인 페이지 오픈소스로 꾸미기 & 회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -11,6 +11,9 @@ const output = {
   login: (req, res) => {
     res.render("./login");
   },
+  register: (req, res) => {
+    res.render("./register");
+  },
 };
 
 const process = {
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -10,4 +10,5 @@ router.get("/", ctrl.output.home);
 router.post("/login", ctrl.process.login);
 router.get("/login", ctrl.output.login);
 
+router.get("/register", ctrl.output.register);
 module.exports = router;
